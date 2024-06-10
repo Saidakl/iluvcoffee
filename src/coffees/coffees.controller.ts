83c5c5f5c6c7d91b3c23418bae5d43b8fdbefadb
Said Akilli <9016890@student.zadkine.nl>
2024-06-10 18:19:35 +0200

    Final?

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,21 +11,23 @@ import {
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeesDto } from './dto/create-coffees.dto';
 import { UpdateCoffeesDto } from './dto/update-coffees.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
-  findAll(@Query() paginationQuery) {
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
     // const { limit, offset } = paginationQuery;
-    return this.coffeesService.findAll();
+    return this.coffeesService.findAll(paginationQuery);
     // return `This action returns all the coffees. Limit: ${limit}, offset: ${offset}`;
   }
 
   @Get(':id')
   findOne(@Param('id') id: number) {
     console.log(typeof id);
-    return this.coffeesService.findOne('' + id);
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
@@ -35,12 +37,12 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeesDto: UpdateCoffeesDto) {
+  update(@Param('id') id: number, @Body() updateCoffeesDto: UpdateCoffeesDto) {
     return this.coffeesService.update(id, updateCoffeesDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     return this.coffeesService.remove(id);
   }
 }
---
+++ src/common/app-data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+
+export default AppDataSource;
